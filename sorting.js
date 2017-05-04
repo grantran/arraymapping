bce1d0de98fe8ed46cb79721cb0a11fc38a0f4b1
Grant Tran <grant.tran@gmail.com>
2017-05-04 19:57:54 +0000

    sorting exercise

---
+++ sorting.js
@@ -0,0 +1,35 @@
+var array = [10, 2, 5, 1, 9];
+
+
+array.sort(function(a,b){
+    return a - b
+})
+
+console.log(array);
+
+//////
+
+var students = [
+  { id: 1, name: "bruce",    age: 40 },
+  { id: 2, name: "zoidberg", age: 22 },
+  { id: 3, name: "alex",     age: 22 },
+  { id: 4, name: "alex",     age: 30 }
+];
+
+students.sort(function (a, b){
+  var nameA = a.name;
+  var nameB = b.name;
+  if (nameA < nameB) {
+    return -1;
+  }
+  if (nameA > nameB) {
+    return 1;
+  }
+  else {
+    var ageA = a.age;
+    var ageB = b.age;
+    return ageA - ageB;
+  }
+})
+
+console.log(students);
